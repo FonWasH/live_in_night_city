0268fbb218d6d04ce8061f7550d0e48ee6538ae6
zimluura <zimluura@gmail.com>
2022-04-04 23:12:33 -0400

    Update Positions.lua

--- constants/Positions.lua
+++ constants/Positions.lua
@@ -1,21 +1,157 @@
 local Positions = {
-    homeBed = {
-        x = -1378.5,
-        y = 1273.0,
+    h10Bed = {
+        x = -1377.4,
+        y = 1275.5,
+		z = 123.1,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = true,
+		couch = false,
+		shower = false
+    },
+    h10Couch = {
+        x = -1378.3,
+        y = 1267.0,
+		z = 122.6,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = true,
+		shower = false
+    },
+    h10Shower = {
+        x = -1383.1,
+        y = 1278.0,
+		z = 123.1,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = false,
+		shower = true
+    },
+    northsideBed = {
+        x = -1503.0,
+        y = 2229.0,
+		z = 22.2,
         xOffset = 1.5,
-        yOffset = 2
-    },
-    homeCouch = {
-        x = -1379.5,
-        y = 1266.7,
-        xOffset = 2,
-        yOffset = 1
-    },
-    homeShower = {
-        x = -1383.2,
-        y = 1277.7,
+        yOffset = 2,
+		zOffset = 0.5,
+		bed = true,
+		couch = false,
+		shower = false
+    },							 -- northside apt doesn't have a couch
+    northsideShower = {
+        x = -1503.0,
+        y = 2227.0,
+		z = 22.2,
         xOffset = 1,
-        yOffset = 0.6
+        yOffset = 0.6,
+		zOffset = 0.5,
+		bed = false,
+		couch = false,
+		shower = true
+    },
+    japantownBed = {
+        x = -784.1,
+        y = 987.5,
+		z = 28.5,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = true,
+		couch = false,
+		shower = false
+    },
+    japantownCouch = {
+        x = -784.5,
+        y = 976.7,
+		z = 28.2,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = true,
+		shower = false
+    },
+    japantownShower = {
+        x = -779.2,
+        y = 972.5,
+		z = 28.3,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = false,
+		shower = true
+    },
+	corpoplazaBed = {
+        x = -1613.7,
+        y = 358.6,
+		z = 49.1,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = true,
+		couch = false,
+		shower = false
+    },
+    corpoplazaCouch = {
+        x = -1601.7,
+        y = 356.4,
+		z = 48.6,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = true,
+		shower = false
+    },
+    corpoplazaShower = {
+        x = -1623.4,
+        y = 353.0,
+		z = 49.3,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = false,
+		shower = true
+    },
+	glenBed = {
+        x = -1524.1,
+        y = -986.4,
+		z = 91.0,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = true,
+		couch = false,
+		shower = false
+    },
+    glenCouch = {
+        x = -1520.7,
+        y = -976.8,
+		z = 86.7,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = true,
+		shower = false
+    },
+    glenShower = {
+        x = -1529.1,
+        y = -969.4,
+		z = 91.2,
+        xOffset = 0.2,
+        yOffset = 0.2,
+		zOffset = 0.2,
+		bed = false,
+		couch = false,
+		shower = true
     }
 }
 
